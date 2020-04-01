b0bb44a621ab49ce534b2d7f894d2b186ce2ca0d
Nursultan <nursuccessmj@google.com>
2020-04-01 16:14:24 +0600

    made accordian dynamic

--- js/main.js
+++ js/main.js
@@ -5,3 +5,20 @@ $(document).ready(function(){
         $('.body').toggleClass('body_lock');     
     });
 })
+
+function openAccordion() {
+    let accordionButtons = document.querySelectorAll('.accordion__button');
+    let accordionHeaders = document.querySelectorAll('.accordion__header');
+    let accordionContents = document.querySelectorAll('.accordion__content');
+
+    for (let i = 0; i < accordionButtons.length; i++) {
+        accordionButtons[i].addEventListener('click', () => {
+            accordionButtons[i].classList.toggle('accordion__button_active');
+            accordionHeaders[i].classList.toggle('accordion__header_active');
+            accordionContents[i].classList.toggle('accordion__content_active');
+        });
+    }
+}
+
+openAccordion();
+
