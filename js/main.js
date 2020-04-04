da2834643bd95225c9099b74217a68fadb7e1546
Nursultan <nursuccessmj@google.com>
2020-04-04 16:37:15 +0600

    fixed accordion, menuLink and sign-upButton hover effect

--- js/main.js
+++ js/main.js
@@ -7,12 +7,14 @@ $(document).ready(function(){
 })
 
 function openAccordion() {
+    let accordionItems = document.querySelectorAll('.accordion__item');
     let accordionButtons = document.querySelectorAll('.accordion__button');
     let accordionHeaders = document.querySelectorAll('.accordion__header');
     let accordionContents = document.querySelectorAll('.accordion__content');
 
     for (let i = 0; i < accordionButtons.length; i++) {
-        accordionButtons[i].addEventListener('click', () => {
+        accordionHeaders[i].addEventListener('click', () => {
+            accordionItems[i].classList.toggle('accordion__item_active');
             accordionButtons[i].classList.toggle('accordion__button_active');
             accordionHeaders[i].classList.toggle('accordion__header_active');
             accordionContents[i].classList.toggle('accordion__content_active');
@@ -22,3 +24,18 @@ function openAccordion() {
 
 openAccordion();
 
+/* function showTeamMember () { 
+    let teamSliderItems = document.querySelectorAll('.team-slider__item');
+    let teamSliderPhotos = document.querySelectorAll('.team-slider__photo');
+
+    for (let i = 0; i < teamSliderItems.length; i++) {
+        let currentItem = document.querySelector('.team-slider__item_active');
+        teamSliderPhotos[i].addEventListener('click', () => {
+            teamSliderItems[i].classList.toggle('team-slider__item_active');
+            currentItem.classList.toggle('team-slider__item_active');
+        });
+    }
+}
+
+showTeamMember();
+ */
